56f2b7e7b7ad8c4e993e1f9402ddb3eb8f114576
Kevin Chun <kchun@tyro.com>
2020-01-08 15:00:27 +1100

    adding a SQS handler to verify the delivery of a message

--- src/main/kotlin/com/example/sqsintegrationtestexample/SqsListener.kt
+++ src/main/kotlin/com/example/sqsintegrationtestexample/SqsListener.kt
@@ -5,9 +5,9 @@ import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener
 import org.springframework.stereotype.Service
 
 @Service
-class SqsListener {
+class SqsListener(val sqsHandler: SqsHandler) {
     @SqsListener(value = ["sqs-integration-test-queue"], deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     fun listen(payload: String) {
-        println("I am the listener and I receive payload : $payload")
+        sqsHandler.handle(payload)
     }
 }
--- src/test/kotlin/com/example/sqsintegrationtestexample/listener/SqsListenerIntegrationTest.kt
+++ src/test/kotlin/com/example/sqsintegrationtestexample/listener/SqsListenerIntegrationTest.kt
@@ -2,10 +2,17 @@ package com.example.sqsintegrationtestexample.listener
 
 import com.amazonaws.services.sqs.AmazonSQS
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder
-import org.junit.jupiter.api.Assertions
+import com.amazonaws.services.sqs.model.QueueAttributeName
+import com.amazonaws.services.sqs.model.SetQueueAttributesRequest
+import com.example.sqsintegrationtestexample.SqsHandler
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.atLeast
+import org.mockito.Mockito.verify
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.test.context.ContextConfiguration
@@ -18,6 +25,9 @@ import org.testcontainers.junit.jupiter.Testcontainers
 @ContextConfiguration(initializers = [SqsListenerIntegrationTest.SqsListenerIntegrationTestInitializer::class])
 @Testcontainers
 class SqsListenerIntegrationTest {
+
+    @MockBean
+    lateinit var sqsHandler: SqsHandler
 
     private lateinit var amazonSQS: AmazonSQS
 
@@ -38,12 +48,18 @@ class SqsListenerIntegrationTest {
 
         // when
         val sendMessageResult = amazonSQS.sendMessage(queueUrl, payload)
-        Assertions.assertNotNull(sendMessageResult.messageId)
+        assertNotNull(sendMessageResult.messageId)
 
         // then
+        // we need some times for listener to consume the message
+        Thread.sleep(500L)
+        // There is no obvious why to directly verify consumption of a message therefore,
+        // We indirectly verify that the expected services have been interacted with
+        verify(sqsHandler, atLeast(1)).handle(payload)
+
         val receiveMessageRequest = amazonSQS.receiveMessage(queueUrl)
         val messages = receiveMessageRequest.messages
-        Assertions.assertTrue(messages.isEmpty())
+        assertTrue(messages.isEmpty())
     }
 
     internal class SqsListenerIntegrationTestInitializer : ApplicationContextInitializer<ConfigurableApplicationContext> {
@@ -52,13 +68,21 @@ class SqsListenerIntegrationTest {
             System.setProperty("AMAZON_REGION", sqsEndpointConfiguration.signingRegion)
             System.setProperty("SQS_ENDPOINT", sqsEndpointConfiguration.serviceEndpoint)
 
-            AmazonSQSClientBuilder
+            val sqs = AmazonSQSClientBuilder
                     .standard()
                     .withEndpointConfiguration(localStackContainer.getEndpointConfiguration(SQS))
                     .withCredentials(localStackContainer.defaultCredentialsProvider)
-                    .build().apply {
-                        this.createQueue(queueName)
-                    }
+                    .build()
+            sqs.createQueue(queueName)
+            val queueUrl: String = sqs.getQueueUrl(queueName).queueUrl
+            val request = SetQueueAttributesRequest()
+                    .withQueueUrl(queueUrl)
+                    .addAttributesEntry(QueueAttributeName.VisibilityTimeout.toString(), visibilityTimeout)
+            sqs.setQueueAttributes(request)
+
+            println("Created queue " + queueName + " with " +
+                    "visibility timeout set to " + visibilityTimeout +
+                    " seconds.")
         }
     }
 
@@ -66,6 +90,7 @@ class SqsListenerIntegrationTest {
         @Container
         val localStackContainer: LocalStackContainer = LocalStackContainer().withServices(SQS)
 
-        private const val queueName: String = "sqs-integration-test-queue"
+        private const val queueName = "sqs-integration-test-queue"
+        private const val visibilityTimeout = "0"
     }
 }
